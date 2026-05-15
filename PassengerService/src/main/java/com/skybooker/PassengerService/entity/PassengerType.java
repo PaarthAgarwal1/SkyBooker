5b0e8a9101324044f865843c8582168d00b587ec
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-05-15 15:21:23 +0530

    [Paarth Agarwal] Clean. the branch to push the modified code

--- PassengerService/src/main/java/com/skybooker/PassengerService/entity/PassengerType.java
+++
@@ -1,7 +0,0 @@
-package com.skybooker.PassengerService.entity;
-
-public enum PassengerType {
-    ADULT,
-    CHILD,
-    INFANT
-}
--- PassengerService/src/main/java/com/skybooker/PassengerService/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.skybooker.PassengerService.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String msg){
-        super(msg);
-    }
-}
